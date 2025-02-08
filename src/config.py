55636e01f21e0853bdcec604505154d922f574f4
Igor <postmander@gmail.com>
2025-02-08 20:36:21 +0300

    добавил dockerfile для приложения и настроил docker-compose с postgres, pgadmin, fastapi_app

--- src/config.py
+++ src/config.py
@@ -1,19 +1,32 @@
-import os 
-from dotenv import load_dotenv
+# import os 
+# from dotenv import load_dotenv
 
 
-load_dotenv()
+# load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+# DB_HOST = os.environ.get("DB_HOST")
+# DB_PORT = os.environ.get("DB_PORT")
+# DB_NAME = os.environ.get("DB_NAME")
+# DB_USER = os.environ.get("DB_USER")
+# DB_PASS = os.environ.get("DB_PASS")
 
-SECRET_AUTH = os.environ.get("SECRET_AUTH")
+# SECRET_AUTH = os.environ.get("SECRET_AUTH")
 
-DB_HOST_TEST = os.environ.get("DB_HOST_TEST") 
-DB_PORT_TEST = os.environ.get("DB_PORT_TEST") 
-DB_NAME_TEST = os.environ.get("DB_NAME_TEST") 
-DB_USER_TEST = os.environ.get("DB_USER_TEST") 
-DB_PASS_TEST = os.environ.get("DB_PASS_TEST") 
+# DB_HOST_TEST = os.environ.get("DB_HOST_TEST") 
+# DB_PORT_TEST = os.environ.get("DB_PORT_TEST") 
+# DB_NAME_TEST = os.environ.get("DB_NAME_TEST") 
+# DB_USER_TEST = os.environ.get("DB_USER_TEST") 
+# DB_PASS_TEST = os.environ.get("DB_PASS_TEST") 
+DB_HOST = "postgres"
+DB_PORT = "5432"
+DB_NAME = "clean_db"
+DB_USER = "admin"
+DB_PASS = "admin"
+
+SECRET_AUTH = "SECRET"
+
+DB_HOST_TEST = "localhost"
+DB_PORT_TEST = 5435
+DB_NAME_TEST = "clean_db_test"
+DB_USER_TEST = "admin"
+DB_PASS_TEST = "admin"
