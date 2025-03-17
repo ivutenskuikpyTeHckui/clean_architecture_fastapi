c5085e6e0e97992bd7d50c60f97f4a2404c9ec3b
Igor <postmander@gmail.com>
2025-03-17 11:57:23 +0300

    bugfix

--- src/config.py
+++ src/config.py
@@ -1,23 +1,23 @@
 import os
 
 
-# def read_secret(secret_name: str, default: str = None) -> str:
-    # secret_path = f"/run/secrets/{secret_name}"
-    # if os.path.exists(secret_path):
-        # with open(secret_path, "r") as f:
-            # return f.read().strip()
+def read_secret(secret_name: str, default: str = None) -> str:
+    secret_path = f"/run/secrets/{secret_name}"
+    if os.path.exists(secret_path):
+        with open(secret_path, "r") as f:
+            return f.read().strip()
 
 
-# DB_HOST = read_secret("db_host")
-# DB_PORT = read_secret("db_port")
-# DB_NAME = read_secret("db_name")
-# DB_USER = read_secret("db_user")
-# DB_PASS = read_secret("db_pass")
-# SECRET_AUTH = read_secret("secret_auth")
+DB_HOST = read_secret("db_host")
+DB_PORT = read_secret("db_port")
+DB_NAME = read_secret("db_name")
+DB_USER = read_secret("db_user")
+DB_PASS = read_secret("db_pass")
+SECRET_AUTH = read_secret("secret_auth")
 
-DB_HOST = "postgres"
-DB_PORT = "5432"
-DB_NAME = "clean_db"
-DB_USER = "admin"
-DB_PASS = "admin"
-SECRET_AUTH = "SECRET"
+# DB_HOST = "postgres"
+# DB_PORT = "5432"
+# DB_NAME = "clean_db"
+# DB_USER = "admin"
+# DB_PASS = "admin"
+# SECRET_AUTH = "SECRET"
---
+++ tests/unit/test_author.py
@@ -0,0 +1,10 @@
+
+
+
+def func(num: int):
+    return 1 / num
+
+
+def test_func():
+    assert func(1) == 1
+    assert func(2) == 0.5
